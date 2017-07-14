789b85db7e6c1c1fe488cf4b0786f9cc1c03c8d0
Felipe Oliveira <felipeweb.programador@gmail.com>
2017-07-14 00:05:23 -0600

    handlerSet must be the last middleware

--- cmd/root.go
+++ cmd/root.go
@@ -52,32 +52,27 @@ func Execute() {
 func app() {
 	n := cfgMiddleware.GetApp()
 	r := router.Get()
-
 	r.HandleFunc("/databases", controllers.GetDatabases).Methods("GET")
 	r.HandleFunc("/schemas", controllers.GetSchemas).Methods("GET")
 	r.HandleFunc("/tables", controllers.GetTables).Methods("GET")
 	r.HandleFunc("/_QUERIES/{queriesLocation}/{script}", controllers.ExecuteFromScripts)
 	r.HandleFunc("/{database}/{schema}", controllers.GetTablesByDatabaseAndSchema).Methods("GET")
-
 	crudRoutes := mux.NewRouter().PathPrefix("/").Subrouter().StrictSlash(true)
-
 	crudRoutes.HandleFunc("/{database}/{schema}/{table}", controllers.SelectFromTables).Methods("GET")
 	crudRoutes.HandleFunc("/{database}/{schema}/{table}", controllers.InsertInTables).Methods("POST")
 	crudRoutes.HandleFunc("/{database}/{schema}/{table}", controllers.DeleteFromTable).Methods("DELETE")
 	crudRoutes.HandleFunc("/{database}/{schema}/{table}", controllers.UpdateTable).Methods("PUT", "PATCH")
-
 	r.PathPrefix("/").Handler(negroni.New(
 		middlewares.AccessControl(),
 		negroni.Wrap(crudRoutes),
 	))
-
 	if config.PrestConf.CORSAllowOrigin != nil {
 		c := cors.New(cors.Options{
 			AllowedOrigins: config.PrestConf.CORSAllowOrigin,
 		})
 		n.Use(c)
 	}
-
+	n.Use(middlewares.HandlerSet())
 	n.UseHandler(r)
 	n.Run(fmt.Sprintf(":%v", config.PrestConf.HTTPPort))
 }
